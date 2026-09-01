a6475a68ab97f274957bdbdc8162abab9775c585
Jmatuu <javierguaman477@gmail.com>
2026-08-31 20:53:05 -0500

    feat: completo ejercicio 1 del bloque A de java

--- bloque_a/Ejercicio1.java
+++ bloque_a/Ejercicio1.java
@@ -1,8 +1,15 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        // TODO: Declara aquí tus variables y asígnales valores
+        // Declaración de variables con los tipos requeridos
+        int edad = 20;
+        double altura = 1.75;
+        String nombre = "Jonnathann";
+        boolean esEstudiante = true;
 
-        // TODO: Muestra los valores por consola
-        
+        // Mostrar los valores por consola
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Es estudiante: " + esEstudiante);
     }
 }
