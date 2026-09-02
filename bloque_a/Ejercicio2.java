aa1df2707410440cd8d1fc4f8ab6b2559fccb5bc
Jmatuu <javierguaman477@gmail.com>
2026-09-01 21:07:32 -0500

    Completado Ejercicio 2 bloque A de Java

--- bloque_a/Ejercicio2.java
+++ bloque_a/Ejercicio2.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        // TODO: Instancia la clase Scanner
+          Scanner leer = new Scanner(System.in);
+        // Lee dos números por teclado
+        System.out.print("Introduce el primer número: ");
+        double num1 = leer.nextDouble();
+        System.out.print("Introduce el segundo número: ");
+        double num2 = leer.nextDouble();
 
-        // TODO: Lee dos números por teclado
+        double suma = num1 + num2;
+        double resta = num1 - num2;
+        double multiplicacion = num1 * num2;
+        double division = num1 / num2;
 
-        // TODO: Calcula y muestra suma, resta, multiplicación y división
-
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+        leer.close(); 
     }
 }
